02eae2cb8c15e4b3304c2c4ccf38fb2b53b5b24a
Yuvraj <fcyuvraj18@gmail.com>
2026-05-12 12:27:55 +0530

    add pool for longer connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,21 @@
-import mysql from "mysql2"
+import mysql from "mysql2";
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  port: process.env.DB_PORT, 
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
-connection.connect((err) => {
+connection.getConnection((err, connection) => {
   if (err) {
     console.log("Database Connection Failed:", err);
   } else {
     console.log("MySQL Connected");
+    connection.release();
   }
 });
 
